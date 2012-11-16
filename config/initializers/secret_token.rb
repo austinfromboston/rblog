7e97fb531190c2f298bc4f584d53b4f87505eff8
Andrew DiMichele & Austin Putman <dev+andrew+austin@omadahealth.com>
2012-11-15 20:17:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rblog::Application.config.secret_token = '4a8326f4e0bb65c20df0887a4ae2ed5bf373ed178609dc1ad0d4273f54963dd8f5474d1c9ae06bf7f3995afd05f33f785a80a9efeb879f22be0f1b8d46bcfa07'
